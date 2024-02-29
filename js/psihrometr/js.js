fa7fa1066849a9756479fd44d0c8b1cf0f569f99
Zelenskiy <Zelensk1973@gmail.com>
2024-02-29 15:43:32 +0200

    fix: random values in psihrometr

--- js/psihrometr/js.js
+++ js/psihrometr/js.js
@@ -39,23 +39,47 @@ pressure = new Array(
   2810,
   3170
 );
+
+const densityDamp = [
+  4.8, 5.2, 5.6, 6.0, 6.4, 6.9, 7.3, 7.8, 8.3, 8.9, 9.4, 10.1, 10.7, 11.4, 12.1,
+  12.9, 13.6, 14.5, 15.4, 16.4, 17.3, 18.4, 19.4, 20.6, 21.8, 23.1, 24.4, 25.8,
+  27.2, 28.8, 30.3,
+];
+
 var damples = [
-  [100, 81, 63, 45, 28, 11, 0, 0, 0, 0, 0],
-  [100, 81, 63, 45, 28, 11, 0, 0, 0, 0, 0],
-  [100, 84, 69, 54, 39, 24, 12, 0, 0, 0, 0],
-  [100, 85, 73, 58, 45, 31, 18, 5, 0, 0, 0],
-  [100, 86, 73, 60, 47, 35, 23, 10, 0, 0, 0],
-  [100, 87, 75, 63, 51, 40, 28, 18, 7, 0, 0],
-  [100, 88, 76, 65, 54, 44, 34, 24, 14, 5, 0],
-  [100, 89, 77, 67, 56, 46, 36, 27, 18, 8, 5],
-  [100, 89, 79, 69, 58, 50, 40, 32, 22, 14, 7],
-  [100, 90, 80, 70, 61, 53, 44, 36, 27, 19, 12],
-  [100, 90, 81, 71, 62, 54, 45, 37, 30, 22, 15],
-  [100, 91, 82, 73, 65, 56, 49, 41, 34, 27, 20],
-  [100, 91, 83, 74, 66, 59, 51, 44, 37, 30, 24],
-  [100, 92, 83, 75, 67, 61, 52, 46, 39, 35, 26],
-  [100, 92, 83, 77, 69, 62, 55, 48, 41, 36, 33],
-  [100, 92, 84, 78, 70, 63, 57, 50, 44, 41, 38],
+  [100, 82, 63, 45, 28, 11, 0, 0, 0, 0, 0, 0],
+  [100, 83, 65, 48, 32, 16, 0, 0, 0, 0, 0, 0],
+  [100, 84, 68, 51, 35, 20, 0, 0, 0, 0, 0, 0],
+  [100, 84, 69, 54, 39, 24, 10, 0, 0, 0, 0, 0],
+  [100, 85, 70, 56, 42, 28, 14, 0, 0, 0, 0, 0],
+  [100, 86, 72, 58, 45, 32, 19, 6, 0, 0, 0, 0],
+  [100, 86, 73, 60, 47, 35, 23, 10, 0, 0, 0, 0],
+  [100, 87, 74, 61, 49, 37, 26, 14, 0, 0, 0, 0],
+  [100, 87, 75, 63, 51, 40, 28, 18, 7, 0, 0, 0],
+  [100, 88, 76, 64, 53, 42, 31, 21, 11, 0, 0, 0],
+  [100, 88, 76, 65, 54, 44, 34, 24, 14, 4, 0, 0],
+  [100, 88, 77, 66, 56, 46, 36, 26, 17, 8, 0, 0],
+  [100, 89, 78, 68, 57, 48, 38, 29, 20, 11, 0, 0],
+  [100, 89, 79, 69, 59, 49, 40, 31, 23, 14, 6, 0],
+  [100, 90, 79, 70, 60, 51, 42, 33, 25, 17, 9, 0],
+  [100, 90, 80, 71, 61, 52, 44, 36, 27, 20, 12, 5],
+  [100, 90, 81, 71, 62, 54, 45, 37, 30, 22, 15, 8],
+  [100, 90, 81, 72, 64, 55, 47, 39, 32, 24, 17, 10],
+  [100, 90, 81, 72, 64, 56, 48, 41, 34, 26, 20, 13],
+  [100, 91, 82, 73, 64, 56, 48, 41, 34, 26, 20, 13],
+  [100, 91, 82, 74, 65, 58, 50, 43, 35, 29, 22, 15],
+  [100, 91, 83, 74, 66, 59, 51, 44, 37, 30, 24, 18],
+  [100, 91, 83, 75, 67, 60, 52, 46, 39, 32, 26, 20],
+  [100, 91, 83, 75, 67, 60, 52, 46, 39, 32, 26, 20],
+  [100, 92, 83, 76, 68, 61, 54, 47, 40, 34, 28, 22],
+  [100, 92, 84, 76, 69, 61, 55, 48, 42, 36, 30, 24],
+  [100, 92, 84, 77, 69, 62, 56, 49, 43, 37, 31, 26],
+  [100, 92, 84, 77, 70, 63, 57, 50, 44, 38, 33, 27],
+  [100, 92, 85, 78, 71, 64, 58, 51, 45, 40, 34, 29],
+  [100, 92, 85, 78, 71, 65, 59, 52, 47, 41, 36, 30],
+  [100, 93, 85, 78, 72, 65, 59, 53, 48, 42, 37, 32],
+  [100, 93, 86, 79, 72, 66, 60, 54, 49, 43, 38, 33],
+  [100, 93, 86, 79, 73, 67, 61, 55, 50, 44, 39, 34],
 ];
 
 ans = new Array();
@@ -237,57 +261,77 @@ function test1() {
 }
 
 function interpolate(value) {
-    const points = [
-      { x: 6, y: 8 },
-      { x: 8, y: 12 },
-      { x: 10, y: 15 },
-      { x: 12, y: 18 },
-      { x: 17, y: 22 },
-    ];
-  
-    // Знаходимо два найближчі опорні значення
-    let i = 0;
-    while (i < points.length - 1 && points[i + 1].x < value) {
-      i++;
-    }
-  
-    const p0 = points[i];
-    const p1 = points[i + 1];
-  
-    // Обчислюємо інтерпольоване значення
-    const t = (value - p0.x) / (p1.x - p0.x);
-    const y = p0.y + t * (p1.y - p0.y);
-  
-    return y;
+  const points = [
+    { y: 7, x: 5 },
+    { y: 9, x: 6 },
+    { y: 10, x: 7 },
+    { y: 12, x: 8 },
+    { y: 14, x: 9 },
+    { y: 15, x: 10 },
+    { y: 20.5, x: 13 },
+    { y: 22.5, x: 14 },
+  ];
+
+  // Знаходимо два найближчі опорні значення
+  let i = 0;
+  while (i < points.length - 1 && points[i + 1].x < value) {
+    i++;
   }
+
+  const p0 = points[i];
+  const p1 = points[i + 1];
+
+  // Обчислюємо інтерпольоване значення
+  const t = (value - p0.x) / (p1.x - p0.x);
+  const y = p0.y + t * (p1.y - p0.y);
+
+  return y;
+}
 
 function getRandomNumber(min, max) {
-    // Випадкове число включно з min та max
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  // Випадкове число включно з min та max
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function init_new() {
-  let numberVolum = getRandomNumber(6, 12);
+  let numberVolum = getRandomNumber(7, 14);
   y = numberVolum;
   if (numberVolum > 4) {
     y = 4;
   }
-  let dt = getRandomNumber(numberVolum - 5, numberVolum-1);
-  console.log('сухий ', interpolate(numberVolum));
-  console.log('вологий ', interpolate(dt));
+  let dt = getRandomNumber(numberVolum - 3, numberVolum - 1);
+  //   console.log('сухий ', (numberVolum));
+  //   console.log('вологий ', (dt));
+
+  const tempDry = Math.round(interpolate(numberVolum));
+  const tempWet = Math.round(interpolate(dt));
+  const dtOri = tempDry - tempWet;
+  console.log("сухий ", tempDry);
+  console.log("вологий ", tempWet);
+  const humidity = damples[tempDry][dtOri];
+  console.log(humidity);
+
+  const densitySaturation = densityDamp[tempDry];
+  const density = (humidity * densitySaturation) / 100;
+
+  document.getElementById("dt_ori").value = `${dtOri}`;
+  document.getElementById("humidity_ori").value = `${humidity}`;
+  document.getElementById("r1c2_ori").value = `${interpolate(numberVolum)}`;
+  document.getElementById("r1c3_ori").value = `${interpolate(dt)}`;
+  document.getElementById("density_ori").value = `${density}`;
+  document.getElementById(
+    "density_saturation_ori"
+  ).value = `${densitySaturation}`;
 
   for (k = 1; k < numberVolum; k++) {
     let level = "u" + k; // выбор ячейки таблицы температуры сухого
     document.getElementById(level).style.background = "#000000";
-    
-    
   }
 
   for (n = 1; n < dt; n++) {
     let level = "u" + (n + 20); // выбор ячейки таблицы температуры влажного
     document.getElementById(level).style.background = "#000000";
   } //-- печать высоты столбика по номеру температуры
-  
 }
 
 init_new();
